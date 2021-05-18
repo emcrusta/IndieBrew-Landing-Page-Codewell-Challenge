f5df46b000b9afcc104ed96fafc45c187cebddde
emcrusta <emcrusta@gmail.com>
2021-05-18 17:18:41 -0600

    update menu style

--- docs/index.js
+++ docs/index.js
@@ -6,11 +6,29 @@ const toggleMenu = () => {
 	console.log(navigation.classList);
 	if (!menuOpen) {
 		navigation.classList.add('open');
+		menuBurger.classList.add('open');
+		navigation.addEventListener('click', toggleMenu);
 		menuOpen = true;
 	} else {
 		navigation.classList.remove('open');
+		menuBurger.classList.remove('open');
+		navigation.removeEventListener('click', toggleMenu);
 		menuOpen = false;
 	}
 };
 
+const closeMenu = () => {
+	if (menuOpen) {
+		if (
+			document.body.scrollTop > 400 ||
+			document.documentElement.scrollTop > 400
+		) {
+			toggleMenu();
+		}
+	}
+};
+
 menuBurger.addEventListener('click', toggleMenu);
+window.onscroll = function () {
+	closeMenu();
+};
